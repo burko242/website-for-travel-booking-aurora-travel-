3996b302d2968769fc50ead676f4908c7bc5bdb9
Ali Burak Serim <burakalibusiness@gmail.com>
2026-05-31 17:19:08 +0300

    Add files via upload
    Here's the SQL file you can use to set up the site's database via PHP MyAdmin...

---
+++ aurora_travel.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 31 May 2026, 16:15:47
+-- Sunucu sürümü: 10.4.32-MariaDB
+-- PHP Sürümü: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `aurora_travel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kullanicilar`
+--
+
+CREATE TABLE `kullanicilar` (
+  `id` int(11) NOT NULL,
+  `kullanici_adi` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `sifre` varchar(255) NOT NULL,
+  `rol` enum('admin','user') DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Tablo döküm verisi `kullanicilar`
+--
+
+INSERT INTO `kullanicilar` (`id`, `kullanici_adi`, `email`, `sifre`, `rol`) VALUES
+(1, 'admin', 'admin@aurora.com', '1234', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `rezervasyonlar`
+--
+
+CREATE TABLE `rezervasyonlar` (
+  `id` int(11) NOT NULL,
+  `kullanici_id` int(11) DEFAULT NULL,
+  `tur_adi` varchar(100) DEFAULT NULL,
+  `kisi_sayisi` int(11) DEFAULT NULL,
+  `rezervasyon_tarihi` date DEFAULT NULL,
+  `durum` varchar(20) DEFAULT 'Beklemede'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `kullanicilar`
+--
+ALTER TABLE `kullanicilar`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kullanici_adi` (`kullanici_adi`);
+
+--
+-- Tablo için indeksler `rezervasyonlar`
+--
+ALTER TABLE `rezervasyonlar`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kullanici_id` (`kullanici_id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kullanicilar`
+--
+ALTER TABLE `kullanicilar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `rezervasyonlar`
+--
+ALTER TABLE `rezervasyonlar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Dökümü yapılmış tablolar için kısıtlamalar
+--
+
+--
+-- Tablo kısıtlamaları `rezervasyonlar`
+--
+ALTER TABLE `rezervasyonlar`
+  ADD CONSTRAINT `rezervasyonlar_ibfk_1` FOREIGN KEY (`kullanici_id`) REFERENCES `kullanicilar` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
